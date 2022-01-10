736d17dc466af5d893fdaf93173e590172ba5072
tuukka <tao.tuominen@gmail.com>
2022-01-10 18:00:22 +0200

    Add admin/teacher page.

---
+++ frontend/src/AdminPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+
+class AdminPage extends React.Component {
+  state = { words: [] };
+  async componentDidMount() {
+    let hr = await fetch("http://localhost:8080/words");
+    let json = await hr.json();
+    this.setState({ words: json });
+  }
+  render() {
+    if (this.state.words.length === 0) {
+      return <p>loading...</p>;
+    } else {
+      let ui = this.state.words.map((word) => (
+        <li key={word.id}>
+          {word.id} - {word.english_word} - {word.finnish_word}
+        </li>
+      ));
+      return <ul>{ui}</ul>;
+    }
+  }
+}
+
+export default AdminPage;
