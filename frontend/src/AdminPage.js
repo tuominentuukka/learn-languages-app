735a2bc6418286cd13059526725b238ce77aeb39
tuukka <tao.tuominen@gmail.com>
2022-01-10 19:27:44 +0200

    Add method for admin to delete word.

--- frontend/src/AdminPage.js
+++ frontend/src/AdminPage.js
@@ -1,24 +1,46 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
 
 class AdminPage extends React.Component {
   state = { words: [] };
   async componentDidMount() {
-    let hr = await fetch("http://localhost:8080/words");
-    let json = await hr.json();
-    this.setState({ words: json });
+    axios.get(`http://localhost:8080/words`).then((res) => {
+      const words = res.data;
+      this.setState({ words });
+    });
   }
+
+  removeWord = (e, word) => {
+    const url = `http://localhost:8080/words/${word.id}`;
+
+    axios
+      .delete(url)
+      .then((res) => {
+        this.setState((previousState) => {
+          return {
+            words: previousState.words.filter((m) => m.id !== word.id),
+          };
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   render() {
-    if (this.state.words.length === 0) {
-      return <p>loading...</p>;
-    } else {
-      let ui = this.state.words.map((word) => (
-        <li key={word.id}>
-          {word.id} - {word.english_word} - {word.finnish_word}
-        </li>
-      ));
-      return <ul>{ui}</ul>;
-    }
+    return (
+      <ul>
+        {this.state.words.map((word) => (
+          <li key={word.id}>
+            {word.english_word} - {word.finnish_word}{" "}
+            <button type="submit" onClick={(e) => this.removeWord(e, word)}>
+              poista sana
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
   }
 }
 
