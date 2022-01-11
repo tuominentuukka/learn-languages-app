82c30558dfecf73f300b3cf9a5c597d7f021d8ac
tuukka <tao.tuominen@gmail.com>
2022-01-11 15:49:41 +0200

    Add variable for url.

--- frontend/src/LearningPage.js
+++ frontend/src/LearningPage.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import axios from "axios";
 
+const url = `http://localhost:8080/words`;
+
 class LearningPage extends React.Component {
   state = { words: [] };
   async componentDidMount() {
-    axios.get(`/words`).then((res) => {
+    axios.get(url).then((res) => {
       const words = res.data;
       this.setState({ words });
     });
