2af17df702460734b8d4f70234965a752fbeb0b1
tuukka <tao.tuominen@gmail.com>
2022-01-12 20:33:19 +0200

    Add value counter for score.

--- frontend/src/LearningPage.js
+++ frontend/src/LearningPage.js
@@ -5,23 +5,63 @@ import axios from "axios";
 const url = `http://localhost:8080/words`;
 
 class LearningPage extends React.Component {
-  state = { words: [] };
-  async componentDidMount() {
+  constructor() {
+    super();
+    this.state = { words: [], answers: [], score: [0] };
+  }
+  //state = { words: [], answers: [] };
+  componentDidMount() {
     axios.get(url).then((res) => {
       const words = res.data;
       this.setState({ words });
     });
   }
+
+  async addAnswer(word) {
+    let rightAnswer = [];
+    let answers = [...this.state.answers];
+    let answer = [...[word.id]];
+    let score = [...this.state.score];
+    answer = this.state.answer;
+    answers[word.id] = await answer;
+    this.setState({ answers });
+    rightAnswer = await this.state.words.find(
+      (word) =>
+        word.id === answers.indexOf(this.state.answers[word.id]) &&
+        word.finnish_word === answer
+    );
+
+    if (rightAnswer.finnish_word === answer) {
+      score[0] = score[0] + 1;
+      this.setState({ score });
+    }
+    console.log(score);
+  }
+
   render() {
     return (
       <div className="content">
         <ul>
           {this.state.words.map((word) => (
             <li key={word.id}>
-              {word.english_word} - <input></input>
+              {word.english_word} -{" "}
+              <input
+                type="text"
+                onChange={(event) => {
+                  this.setState({ answer: event.target.value });
+                }}
+                onKeyPress={(event) => {
+                  if (event.key === "Enter") {
+                    this.addAnswer(word);
+                  }
+                }}
+              ></input>
             </li>
           ))}
         </ul>
+        <h3>
+          score = {this.state.score[0]}/{this.state.words.length}
+        </h3>
       </div>
     );
   }
