c62eb65277d3917a1a7b075d18d868e1f57c47ca
tuukka <tao.tuominen@gmail.com>
2022-01-13 22:11:32 +0200

    Edit url.

--- frontend/src/LearningPage.js
+++ frontend/src/LearningPage.js
@@ -26,7 +26,8 @@ const port = process.env.PORT || 8080;
  * Url to backend.
  * @const {String}
  */
-const url = `http://localhost:${port}/words`;
+//const url = `http://localhost:${port}/words`;
+const url = `/words`;
 
 /**
  * Styling for table cells.
