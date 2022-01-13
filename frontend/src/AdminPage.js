0e777353374cc9743728bdcaf702885d6435e4cd
tuukka <tao.tuominen@gmail.com>
2022-01-13 14:06:33 +0200

    Add materialUI design.

--- frontend/src/AdminPage.js
+++ frontend/src/AdminPage.js
@@ -1,8 +1,42 @@
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const url = `http://localhost:8080/words`;
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+const style = {
+  maxWidth: "70px",
+  textAlign: "center",
+  margin: "auto",
+};
 
 class AdminPage extends React.Component {
   constructor() {
@@ -85,26 +119,51 @@ class AdminPage extends React.Component {
       <div className="body">
         <div className="content">
           <div className="list">
-            <h1>Lisää sana tai muokkaa haluamaasi</h1>
-            <ul>
-              {this.state.words.map((word) => (
-                <li key={word.id}>
-                  {word.english_word} - {word.finnish_word}{" "}
-                  <button
-                    type="submit"
-                    onClick={(e) => this.removeWord(e, word)}
-                  >
-                    poista sana
-                  </button>
-                  <button
-                    type="submit"
-                    onClick={(e) => this.updateWord(e, word)}
-                  >
-                    päivitä sanaa
-                  </button>
-                </li>
-              ))}
-            </ul>
+            <TableContainer
+              component={Paper}
+              style={{ maxWidth: "800px", textAlign: "center", margin: "auto" }}
+            >
+              <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell style={style}>Englanniksi</StyledTableCell>
+                    <StyledTableCell align="right" style={style}>
+                      Suomeksi
+                    </StyledTableCell>
+                    <StyledTableCell style={style}></StyledTableCell>
+                    <StyledTableCell style={style}></StyledTableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {this.state.words.map((word) => (
+                    <StyledTableRow key={word.id}>
+                      <StyledTableCell component="th" scope="row" style={style}>
+                        {word.english_word}
+                      </StyledTableCell>
+                      <StyledTableCell align="right" style={style}>
+                        {word.finnish_word}
+                      </StyledTableCell>
+                      <StyledTableCell align="right" style={style}>
+                        <button
+                          type="submit"
+                          onClick={(e) => this.removeWord(e, word)}
+                        >
+                          poista
+                        </button>
+                      </StyledTableCell>
+                      <StyledTableCell align="right" style={style}>
+                        <button
+                          type="submit"
+                          onClick={(e) => this.updateWord(e, word)}
+                        >
+                          päivitä
+                        </button>
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </div>
           <div className="form">
             <form onSubmit={this.onSubmit}>
