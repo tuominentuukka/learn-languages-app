aeb429c47ec3fa72ee42bae0f99574ad5a1524cd
tuukka <tao.tuominen@gmail.com>
2022-01-14 02:45:06 +0200

    Edit JSDoc.

--- frontend/src/LearningPageFinnish.js
+++ frontend/src/LearningPageFinnish.js
@@ -60,8 +60,8 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 /**
- * @class LearningPage
- * Represents a LearningPage.
+ * @class LearningPageFinnish
+ * Represents a LearningPageFinnish.
  * <br>class to build learningPage component
  * @extends React.Component
  */
